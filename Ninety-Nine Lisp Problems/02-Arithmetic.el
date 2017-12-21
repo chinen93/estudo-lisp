065f619232c5655035c9830c0537624b22e2046c
Pedro Chinen <chinen93@gmail.com>
2017-12-21 20:08:24 -0200

    Formatar os exercicios com 70 caracteres

--- Ninety-Nine Lisp Problems/02-Arithmetic.el
+++ Ninety-Nine Lisp Problems/02-Arithmetic.el
@@ -5,7 +5,8 @@
 ; * (is-prime 7)
 ; T
 
-; P32 (**) Determine the greatest common divisor of two positive integer numbers.
+; P32 (**) Determine the greatest common divisor of two positive integer
+; numbers.
 ; Use Euclid's algorithm.
 ; Example:
 ; * (gcd 36 63)
@@ -18,13 +19,19 @@
 ; T
 
 ; P34 (**) Calculate Euler's totient function phi(m).
-; Euler's so-called totient function phi(m) is defined as the number of positive integers r (1 <= r < m) that are coprime to m.
-; Example: m = 10: r = 1,3,7,9; thus phi(m) = 4. Note the special case: phi(1) = 1.
+; Euler's so-called totient function phi(m) is defined as the number of
+; positive integers r (1 <= r < m) that are coprime to m.
+; Example: m = 10: r = 1,3,7,9; thus phi(m) = 4. Note the special case:
+; phi(1) = 1.
 ;
 ; * (totient-phi 10)
 ; 4
 ;
-; Find out what the value of phi(m) is if m is a prime number. Euler's totient function plays an important role in one of the most widely used public key cryptography methods (RSA). In this exercise you should use the most primitive method to calculate this function (there are smarter ways that we shall discuss later).
+; Find out what the value of phi(m) is if m is a prime number. Euler's
+; totient function plays an important role in one of the most widely used
+; public key cryptography methods (RSA). In this exercise you should use the
+; most primitive method to calculate this function (there are smarter ways
+; that we shall discuss later).
 ;
 
 ; P35 (**) Determine the prime factors of a given positive integer.
@@ -42,27 +49,41 @@
 ;
 
 ; P37 (**) Calculate Euler's totient function phi(m) (improved).
-; See problem P34 for the definition of Euler's totient function. If the list of the prime factors of a number m is known in the form of problem P36 then the function phi(m) can be efficiently calculated as follows: Let ((p1 m1) (p2 m2) (p3 m3) ...) be the list of prime factors (and their multiplicities) of a given number m. Then phi(m) can be calculated with the following formula:
+; See problem P34 for the definition of Euler's totient function. If the list
+; of the prime factors of a number m is known in the form of problem P36 then
+; the function phi(m) can be efficiently calculated as follows:
+;Let ((p1 m1) (p2 m2) (p3 m3) ...) be the list of prime factors (and their
+; multiplicities) of a given number m. Then phi(m) can be calculated with the
+; following formula:
 ; phi(m) = (p1 - 1) * p1 ** (m1 - 1) + (p2 - 1) * p2 ** (m2 - 1) + (p3 - 1) * p3 ** (m3 - 1) + ...
 ;
 ; Note that a ** b stands for the b'th power of a.
 ;
 
 ; P38 (*) Compare the two methods of calculating Euler's totient function.
-; Use the solutions of problems P34 and P37 to compare the algorithms. Take the number of logical inferences as a measure for efficiency. Try to calculate phi(10090) as an example.
+; Use the solutions of problems P34 and P37 to compare the algorithms. Take
+; the number of logical inferences as a measure for efficiency. Try to
+; calculate phi(10090) as an example.
 
 ; P39 (*) A list of prime numbers.
-; Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.
+; Given a range of integers by its lower and upper limit, construct a list of
+; all prime numbers in that range.
 
 ; P40 (**) Goldbach's conjecture.
-; Goldbach's conjecture says that every positive even number greater than 2 is the sum of two prime numbers. Example: 28 = 5 + 23. It is one of the most famous facts in number theory that has not been proved to be correct in the general case. It has been numerically confirmed up to very large numbers (much larger than we can go with our Prolog system). Write a predicate to find the two prime numbers that sum up to a given even integer.
+; Goldbach's conjecture says that every positive even number greater than
+; 2 is the sum of two prime numbers. Example: 28 = 5 + 23. It is one of the
+; most famous facts in number theory that has not been proved to be correct
+; in the general case. It has been numerically confirmed up to very large
+; numbers (much larger than we can go with our Prolog system). Write a
+; predicate to find the two prime numbers that sum up to a given even integer.
 ; Example:
 ; * (goldbach 28)
 ; (5 23)
 ;
 
 ; P41 (**) A list of Goldbach compositions.
-; Given a range of integers by its lower and upper limit, print a list of all even numbers and their Goldbach composition.
+; Given a range of integers by its lower and upper limit, print a list of all
+; even numbers and their Goldbach composition.
 ; Example:
 ; * (goldbach-list 9 20)
 ; 10 = 3 + 7
@@ -72,7 +93,10 @@
 ; 18 = 5 + 13
 ; 20 = 3 + 17
 ;
-; In most cases, if an even number is written as the sum of two prime numbers, one of them is very small. Very rarely, the primes are both bigger than say 50. Try to find out how many such cases there are in the range 2..3000.
+; In most cases, if an even number is written as the sum of two prime
+; numbers, one of them is very small. Very rarely, the primes are both bigger
+; than say 50. Try to find out how many such cases there are in the
+; range 2..3000.
 ;
 ; Example (for a print limit of 50):
 ; * (goldbach-list 1 2000 50)
