660f4f23ed55b970e94b6c9ce831a304bf71839a
Pedro Chinen <chinen93@gmail.com>
2018-01-18 23:15:41 -0200

    Beginning of loop and recursion study

---
+++ loop-recursion.el
@@ -0,0 +1,7 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Loop and Recursion
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;; References
+;;;; https://www.gnu.org/software/emacs/manual/html_node/eintr/Loops-_0026-Recursion.html
+;;;; 
