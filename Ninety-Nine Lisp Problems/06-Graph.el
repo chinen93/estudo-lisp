00ee1879f2cf85d1422bdc8b441057b99ccd12d2
Pedro Chinen <chinen93@gmail.com>
2018-02-25 09:42:04 -0300

    Algorithm for transforming from graph term to others

--- Ninety-Nine Lisp Problems/06-Graph.el
+++ Ninety-Nine Lisp Problems/06-Graph.el
@@ -148,6 +148,25 @@
 ;;;; if (null found)
 ;;;;;; append (list v1 [v2]) to result
 
+;; convert-graph-term-to-edge graph
+;; let edges (second graph)
+;; return edges
+
+;; convert-graph-term-to-adjacency graph
+;; let vertices (first graph)
+;; let edges (second graph) 
+;; let nodes () 
+;; for each vertice of vertices
+;;;; append (vertice []) to nodes
+;; for each edge of edges
+;;;; let v1 (first edge)
+;;;; let v2 (second edge)
+;;;; search for v1 on nodes list
+;;;;;; append v2 to the vector of the correspondent node
+
+
+
+
 
 ; P81 (**) Path from one node to another one
 ; Write a predicate path(G,A,B,P) to find an acyclic path P from node A to
